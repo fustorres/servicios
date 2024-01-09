1e5a8c41bce62a923e60ccde179434b4a26907ee
unknown <fustorres.fuscontacam.@gmail.com>
2024-01-09 14:42:06 -0500

    Subiendo servicios

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Servicios
 
 Reparación y mantenimiento de computadores
+
+## Services
+
+- [Landing page de tipo servicios](https://fustorres.github.io/servicios/news)
---
+++ news/recommendations/src/main.js
@@ -0,0 +1,6 @@
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./lib/index.production.js');
+  } else {
+    module.exports = require('./lib/index.development.js');
+  }
+  
