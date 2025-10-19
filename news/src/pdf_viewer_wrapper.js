5afccedd9d442d92241bb61cf896bc708cf8c43b
unknown <fuscontacam@gmail.com>
2025-10-19 11:23:13 -0500

    actulizando

--- news/src/pdf_viewer_wrapper.js
+++
@@ -1,7 +0,0 @@
-import html2pdf from 'html2pdf.js';
-
-export function pdf_viewer_wrapper(sectionElement){
-    const element = document.getElementById(sectionElement)
-
-    html2pdf().from(element).save()
-}
