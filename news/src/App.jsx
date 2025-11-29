2e62542fb6bca233d21984c75452809820e2c1f3
unknown <fuscontacam@gmail.com>
2025-11-29 14:06:59 -0500

    actulizando

---
+++ news/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+//import ContactForm from "./components/ContactForm";
+//import CrudApi from "./components/CrudApi";
+//import CrudApp from "./components/CrudApp";
+import Modals from "./components/Modals";
+//import SelectsAnidados from "./components/SelectsAnidados";
+//import SongSearch from "./components/SongSearch";
+
+function App() {
+  return (
+    <>      
+      
+      <Modals />
+      
+      {/* <ContactForm />
+      <hr />
+      <SelectsAnidados />
+      <hr />
+      <SongSearch />
+      <hr />
+      <CrudApi />
+      <hr /> 
+      <CrudApp /> */} 
+    </>
+  );
+}
+
+export default App;
